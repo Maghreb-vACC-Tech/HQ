21a79f5bd20be0222ae2143336fcebe34d1bfee6
Ilyass Baba <ilyassbaba.me@gmail.com>
2023-06-27 14:50:54 +0100

    added annoucement and have a working controller with api

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,13 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'app_dashboard')]
     public function index(): Response
     {
+
         return $this->render('dashboard/index.html.twig', [
-            
         ]);
     }
 }
