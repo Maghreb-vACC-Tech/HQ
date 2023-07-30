5315c5c5fc66dfa7ecee15c79bee2466130be8d1
ilyassbaba <ilyassbaba.me@gmail.com>
2023-07-30 11:30:51 +0100

    booking

---
+++ migrations/Version20230729170103.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230729170103 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE announcement (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, date VARCHAR(255) NOT NULL, subject VARCHAR(255) DEFAULT NULL, message LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE trainee (id INT AUTO_INCREMENT NOT NULL, cid VARCHAR(255) NOT NULL, rating VARCHAR(255) NOT NULL, comment VARCHAR(255) NOT NULL, mentor VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE announcement');
+        $this->addSql('DROP TABLE trainee');
+    }
+}
